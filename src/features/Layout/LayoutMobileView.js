03a80abc161e6390391c892e6395f5ca0b60b996
Enver-PC\Enver <envercanyilmaz@gmail.com>
2022-11-22 22:41:02 +0300

    list render performance with react-virtualized

--- src/features/Layout/LayoutMobileView.js
+++ src/features/Layout/LayoutMobileView.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { List, AutoSizer } from 'react-virtualized';
 import BetBasket from '../BetBasket';
 import './layout.scss';
 
@@ -13,6 +14,42 @@ const LayoutMobileView = ({
 }) => {
   const [showBetBasket, setShowBetBasket] = useState(false);
 
+  // Using react-virtualized for list rendering performance
+  const renderRow = ({ list, index, style }) => {
+    const item = list[index];
+    return (
+      <div className='betting-section-mobile' key={item.C} style={style}>
+        <div className='betting-row-mobile'>
+          <div className='cell' type='id'>{item.C}</div>
+          <div className='cell' type='date'>{item.D}</div>
+          <div className='cell' type='time'>{item.T}</div>
+          <div className='cell' type='name'>{item.N}</div>
+        </div>
+        <div className='betting-header-row-mobile' >
+          <div></div>
+          <div className='mobile-header'>1</div>
+          <div className='mobile-header'>x</div>
+          <div className='mobile-header'>2</div>
+          <div></div>
+          <div className='mobile-header'>Alt</div>
+          <div className='mobile-header'>Üst</div>
+        </div>
+        <div className='betting-row-mobile'>
+          <div className='cell' type='mbs'>{item.OCG[bettingIndexMap.matchResult]?.MBS || '4'}</div>
+          <div className='btn-cell' data-bet-type='1' data-bet-value='0'>{item.OCG[bettingIndexMap.matchResult]?.OC[0]?.O || '-'}</div>
+          <div className='btn-cell' data-bet-type='1' data-bet-value='1'>{item.OCG[bettingIndexMap.matchResult]?.OC[1]?.O || '-'}</div>
+          <div className='btn-cell' data-bet-type='1' data-bet-value='2'>{item.OCG[bettingIndexMap.matchResult]?.OC[2]?.O || '-'}</div>
+
+          <div className='cell' type='mbs'>{item.OCG[bettingIndexMap.overUnder]?.MBS || '4'}</div>
+          <div className='btn-cell' data-bet-type='5' data-bet-value='25'>{item.OCG[bettingIndexMap.overUnder]?.OC[25]?.O || '-'}</div>
+          <div className='btn-cell' data-bet-type='5' data-bet-value='26'>{item.OCG[bettingIndexMap.overUnder]?.OC[26]?.O || '-'}</div>
+
+          <div className='btn-cell-last'>+99</div>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div className='layout-mobile'>
       <div className='header'>{`Event Count: ${eventCount}`}</div>
@@ -24,37 +61,16 @@ const LayoutMobileView = ({
               <div className='header-text'>{comp}</div>
             </div>
             <div onClick={handleBettingAreaClick}>
-              {list?.length ? list.map((item) => (
-                <div className='betting-section-mobile' key={item.C}>
-                  <div className='betting-row-mobile'>
-                    <div className='cell' type='id'>{item.C}</div>
-                    <div className='cell' type='date'>{item.D}</div>
-                    <div className='cell' type='time'>{item.T}</div>
-                    <div className='cell' type='name'>{item.N}</div>
-                  </div>
-                  <div className='betting-header-row-mobile' >
-                    <div></div>
-                    <div className='mobile-header'>1</div>
-                    <div className='mobile-header'>x</div>
-                    <div className='mobile-header'>2</div>
-                    <div></div>
-                    <div className='mobile-header'>Alt</div>
-                    <div className='mobile-header'>Üst</div>
-                  </div>
-                  <div className='betting-row-mobile'>
-                    <div className='cell' type='mbs'>{item.OCG[bettingIndexMap.matchResult]?.MBS || '4'}</div>
-                    <div className='btn-cell' data-bet-type='1' data-bet-value='0'>{item.OCG[bettingIndexMap.matchResult]?.OC[0]?.O || '-'}</div>
-                    <div className='btn-cell' data-bet-type='1' data-bet-value='1'>{item.OCG[bettingIndexMap.matchResult]?.OC[1]?.O || '-'}</div>
-                    <div className='btn-cell' data-bet-type='1' data-bet-value='2'>{item.OCG[bettingIndexMap.matchResult]?.OC[2]?.O || '-'}</div>
 
-                    <div className='cell' type='mbs'>{item.OCG[bettingIndexMap.overUnder]?.MBS || '4'}</div>
-                    <div className='btn-cell' data-bet-type='5' data-bet-value='25'>{item.OCG[bettingIndexMap.overUnder]?.OC[25]?.O || '-'}</div>
-                    <div className='btn-cell' data-bet-type='5' data-bet-value='26'>{item.OCG[bettingIndexMap.overUnder]?.OC[26]?.O || '-'}</div>
-
-                    <div className='btn-cell-last'>+99</div>
-                  </div>
-                </div>
-              )) : null}
+               <AutoSizer>
+                {({ width, height }) => <List
+                  width={width}
+                  height={height}
+                  rowRenderer={({ style, index }) => renderRow({ list, index, style })}
+                  rowCount={list.length}
+                  rowHeight={82}
+                />}
+              </AutoSizer>
             </div>
           </div>
         );
